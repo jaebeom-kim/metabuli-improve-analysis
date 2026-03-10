721834fd3bdb910acca06c144a9795a5870a9466
JaebeomKim0731 <jbeom0731@gmail.com>
2026-03-10 19:09:49 +0900

    syncmer + min score

---
+++ run_metabuli_syncmer+P_exclusions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# s-mer 6
+# Subspecies exclusion test
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        subsp-ex \
+        --threads 64 \
+        --min-cons-cnt 2 \
+        --min-score 0.2 \
+        --max-shift 8 > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6/subsp-ex.log
+
+# Species exclusion test
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        sp-ex \
+        --threads 64 \
+        --min-cons-cnt 2 \
+        --min-score 0.2 \
+        --max-shift 8 > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6/sp-ex.log
+
+
+# Genus exclusion test
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/genus-ex-small_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/genus-ex-small_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        genus-ex \
+        --threads 64 \
+        --min-cons-cnt 2 \
+        --min-score 0.2 \
+        --max-shift 8 > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6/genus-ex.log
+
+# Family exclusion test
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/family-ex-small_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/family-ex-small_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        family-ex \
+        --threads 64 \
+        --min-cons-cnt 2 \
+        --min-score 0.2 \
+        --max-shift 8 > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6/family-ex.log
