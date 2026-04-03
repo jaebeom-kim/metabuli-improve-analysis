2175013f200bd50987923cafadca35170bcaca80
JaebeomKim0731 <jbeom0731@gmail.com>
2026-04-04 00:52:18 +0900

    table2

--- table2.sh
+++ table2.sh
@@ -1,102 +1,94 @@
 #!/bin/bash
 
-# # Inclusion
+# Inclusion
 
-# # standard
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-#         sp-in-0404 \
-#         --threads 64 
+# standard
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+        sp-in-0404 \
+        --threads 64 
 
-# # reduced 8 11 0
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
-#         sp-in-0404 \
-#         --threads 64
+# reduced 8 11 0
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
+        sp-in-0404 \
+        --threads 64
 
-# # reduced 9 11 0
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         sp-in-0404 \
-#         --min-aa 11 \
-#         --threads 64
+# reduced 9 11 0
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        sp-in-0404-11 \
+        --min-aa 11 \
+        --threads 64
 
-# # reduced 9 12 0
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         sp-in-0404 \
-#         --min-aa 12 \
-#         --threads 64
+# reduced 9 12 0
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        sp-in-0404-12 \
+        --min-aa 12 \
+        --threads 64
 
-# # standard 8 11 2  
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-1110111011-re \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-1110111011-re \
-#         sp-in-0404 \
-#         --min-aa 11 \
-#         --threads 64
+# standard 8 11 2  
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-1110111011-re \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-1110111011-re \
+        sp-in-0404 \
+        --min-aa 11 \
+        --threads 64
 
-# # reduced 9 12 2
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
-#         /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
-#         sp-in-0404 \
-#         --threads 64
 
-# # Exclusion
+# Exclusion
 
-# # standard
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-#         sp-ex-0404 \
-#         --threads 64    
+# standard
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+        sp-ex-0404 \
+        --threads 64    
 
-# # reduced 8 11 0
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
-#         sp-ex-0404 \
-#         --threads 64
+# reduced 8 11 0
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa-8mer \
+        sp-ex-0404 \
+        --threads 64
 
-# # reduced 9 11 0
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         sp-ex-0404 \
-#         --threads 64
+# reduced 9 11 0
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        sp-ex-0404-11 \
+        --threads 64
 
-# # reduced 9 12 0
-# ~/dev/Metabuli/release/src/metabuli classify \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-#         sp-ex-0404 \
-#         --min-aa 12 \
-#         --threads 64
+# reduced 9 12 0
+~/dev/Metabuli/release/src/metabuli classify \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
+        sp-ex-0404-12 \
+        --min-aa 12 \
+        --threads 64
 
 # standard 8 11 2
 ~/dev/Metabuli/release/src/metabuli classify \
@@ -107,12 +99,3 @@
         sp-ex-0404 \
         --threads 64
 
-# reduced 9 12 2
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-reducedaa \
-        sp-ex-0404 \
-        --min-aa 12 \
-        --threads 64
