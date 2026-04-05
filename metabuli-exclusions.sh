b91a4f1afbe03a328d3e1b4884a29c422e176170
JaebeomKim0731 <jbeom0731@gmail.com>
2026-04-05 16:58:30 +0900

    redo exclusions only for --preice 1

--- metabuli-exclusions.sh
+++ metabuli-exclusions.sh
@@ -2,24 +2,24 @@
 
 # subspecies exclusion test
 
-# Metabuli-New
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
-        subsp-ex-0405 \
-        --threads 32
+# # Metabuli-New
+# ~/dev/Metabuli/release/src/metabuli classify \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_1.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_2.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
+#         subsp-ex-0405 \
+#         --threads 32
 
-# Metabuli-Light
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
-        subsp-ex-light-0405 \
-        --syncmer 1 --smer-len 6 \
-        --threads 32
+# # Metabuli-Light
+# ~/dev/Metabuli/release/src/metabuli classify \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_1.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-ex-small_2.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
+#         subsp-ex-light-0405 \
+#         --syncmer 1 --smer-len 6 \
+#         --threads 32
 
 # Metabuli-Precise
 ~/dev/Metabuli/release/src/metabuli classify \
@@ -34,24 +34,24 @@
 
 # species exclusion test
 
-# Metabuli-New
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
-        sp-ex-0405 \
-        --threads 32
+# # Metabuli-New
+# ~/dev/Metabuli/release/src/metabuli classify \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
+#         sp-ex-0405 \
+#         --threads 32
 
-# Metabuli-Light
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
-        sp-ex-light-0405 \
-        --syncmer 1 --smer-len 6 \
-        --threads 32
+# # Metabuli-Light
+# ~/dev/Metabuli/release/src/metabuli classify \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_1.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-ex-100_2.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-ex-0405 \
+#         sp-ex-light-0405 \
+#         --syncmer 1 --smer-len 6 \
+#         --threads 32
 
 # Metabuli-Precise
 ~/dev/Metabuli/release/src/metabuli classify \
