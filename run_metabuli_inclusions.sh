d94d28ceafc4dd9ac22434b4aaf4fe559d4e7e26
JaebeomKim0731 <jbeom0731@gmail.com>
2026-04-05 16:53:29 +0900

    inclusions

--- run_metabuli_inclusions.sh
+++ run_metabuli_inclusions.sh
@@ -1,97 +1,82 @@
 #!/bin/bash
 
 # Subspecies inclusion test
-# Metabuli Old
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-        subsp-in \
-        --threads 64 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db/subsp-in.log
+# # Metabuli Old
+# ~/dev/Metabuli/release/src/metabuli classify \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_1.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_2.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+#         subsp-in \
+#         --threads 64 \
+#         > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db/subsp-in.log
 
 # Metabuli New
 ~/dev/Metabuli/release/src/metabuli classify \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_1.fna \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
         subsp-in \
-        --threads 64 \
-        --min-cons-cnt 2 \
-        --max-shift 4 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111/subsp-in.log
+        --threads 64 
 
 # Metabuli Precise
 ~/dev/Metabuli/release/src/metabuli classify \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_1.fna \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
-        subsp-in+0.2 \
+        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
+        subsp-in-precise \
         --threads 64 \
-        --min-score 0.2 \
-        --min-cons-cnt 2 \
-        --max-shift 4 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111/subsp-in+0.2.log
+        --precise 1 
 
 # Metabuli Light
 ~/dev/Metabuli/release/src/metabuli classify \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_1.fna \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/subsp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
         subsp-in-light \
         --threads 64 \
-        --min-cons-cnt 2 \
-        --max-shift 8 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6/subsp-in-light.log
+        --syncmer 1 --smer-len 6
+
 
 # Species inclusion test
 # Metabuli Old
-~/dev/Metabuli/release/src/metabuli classify \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
-        sp-in \
-        --threads 64 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db/sp-in.log
+# ~/dev/Metabuli/release/src/metabuli classify \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db \
+#         /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
+#         sp-in \
+#         --threads 64 \
+#         > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db/sp-in.log
 
 # Metabuli New
 ~/dev/Metabuli/release/src/metabuli classify \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
         sp-in \
-        --threads 64 \
-        --min-cons-cnt 2 \
-        --max-shift 4 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111/sp-in.log
+        --threads 64 
 
 # Metabuli Precise
 ~/dev/Metabuli/release/src/metabuli classify \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111 \
-        sp-in+0.2 \
+        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
+        sp-in-precise \
         --threads 64 \
-        --min-score 0.2 \
-        --min-cons-cnt 2 \
-        --max-shift 4 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111/sp-in+0.2.log
+        --precise 1 
 
 # Metabuli Light
 ~/dev/Metabuli/release/src/metabuli classify \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_1.fna \
         /fast/jaebeom/benchmark/gtdb-genomes/simulated_reads/sp-in-100_2.fna \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
-        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6 \
+        /fast/jaebeom/benchmark/gtdb-genomes/11101110111 \
+        /fast/jaebeom/benchmark/gtdb-genomes/metabuli-res-0405 \
         sp-in-light \
         --threads 64 \
-        --min-cons-cnt 2 \
-        --max-shift 8 \
-        > /fast/jaebeom/benchmark/gtdb-genomes/metabuli-db-11101110111-s6/sp-in-light.log
+        --syncmer 1 --smer-len 6
